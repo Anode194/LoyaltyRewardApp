ad91d6a7bece5b2d596936393341b6d8516056f6
Jerry Phillips <jerrycphillips@mac.com>
2018-01-02 15:27:12 -0700

    - Created mockup of Beans & Brews launch screen and open collection view.

---
+++ LaBarba/IntroCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  IntroCollectionViewCell.swift
+//  LaBarba
+//
+//  Created by Jerry Phillips on 1/2/18.
+//  Copyright © 2018 Jerry Phillips. All rights reserved.
+//
+
+import UIKit
+
+class IntroCollectionViewCell: UICollectionViewCell {
+    
+  @IBOutlet weak var image: UIImageView!
+  
+}
--- LaBarba/ViewController.swift
+++ LaBarba/ViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+  
+  @IBOutlet weak var btnSkip:UIButton!
+  @IBOutlet weak var collectionView: UICollectionView!
+  let imageFiles = ["first.png", "second.png", "third.png", "fourth.png", "fifth.png"]
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    collectionView.dataSource = self
+    collectionView.delegate = self
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
+  
+  @IBAction func btnSkipDidPress(_ sender: Any) {
+    
+  }
+  
+  //MARK: CollectionViewDelegate Methods
+  func numberOfSections(in collectionView: UICollectionView) -> Int {
+    return 1
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 5
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! IntroCollectionViewCell
+    cell.image.image = UIImage(named: imageFiles[indexPath.item])
+    print(cell.frame.size.width)
+    print(cell.frame.size.height)
+    return cell
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+  }
+  
 }
 
